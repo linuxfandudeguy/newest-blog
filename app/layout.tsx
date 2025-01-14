ecd988decbb54d295afbd3ab3cbc8a8e70fe35c5
lelbois <164905463+linuxfandudeguy@users.noreply.github.com>
2025-01-14 18:12:53 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,17 +18,16 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300`}>
         <Providers>{children}</Providers>
-       <div>
+        <div className="flex items-center justify-center">
       <a href="/rss">
         <img 
-          src="/rss-svgrepo-com.svg" 
+          src="/rss-svgrepo-com.png" 
           alt="RSS Icon" 
-          width="64" 
-          height="64"
+          className="w-16 h-16 rounded-lg" // Tailwind styles
         />
-      </a>
-    </div>    
-   </body>
+      </a> 
+    </div>   
+  </body>
     </html>
   )
 }
